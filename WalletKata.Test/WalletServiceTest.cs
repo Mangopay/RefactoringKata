006dbf9e9fa0a8955c07f2c0eb22ef91c7b07607
beedi <bdiemert@gmail.com>
2017-02-13 11:50:20 +0100

    Added Tests for User Logging

--- WalletKata.Test/WalletServiceTest.cs
+++ WalletKata.Test/WalletServiceTest.cs
@@ -1,13 +1,32 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using NSubstitute;
+using NUnit.Framework;
+using WalletKata.Exceptions;
+using WalletKata.Users;
+using WalletKata.Wallets;
 
 namespace WalletKata.Test
 {
     public class WalletServiceTest
     {
         [Test]
-        public void TestSomething()
+        public void UserLoggingFailedTest()
         {
-            
+            var wService = new WalletService();
+            var uS = Substitute.For<IGetLoggedUser>();
+            uS.GetLoggedUser().Returns((User) null);
+
+            Assert.Throws<UserNotLoggedInException>(() => wService.GetWalletsByUser(null, uS));
+        }
+
+        [Test]
+        public void UserLoggingPassedTest()
+        {
+            var wService = new WalletService();
+            var uS = Substitute.For<IGetLoggedUser>();
+            uS.GetLoggedUser().Returns(new User());
+
+            Assert.DoesNotThrow(() => wService.GetWalletsByUser(new User(), uS));
         }
     }
 }
